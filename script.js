849b6f5c7bfdc3231c37156a6cd5ececbb015288
selimistani <142060673+selimistani@users.noreply.github.com>
2024-09-06 17:59:23 +0400

    mistake

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".gradient-btn").addEventListener("click", function () {
+  const toggleContainer = document.querySelector(".toggle-container");
+  toggleContainer.classList.toggle("hidden");
+});
